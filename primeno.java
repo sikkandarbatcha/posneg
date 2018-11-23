2352a86d41eaaf8a562b582d3084a217ddc7fa87
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2018-11-23 02:31:02 -0800

    Create primeno.java

---
+++ primeno.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class primeno
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n%2!=0)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}	
