677d0ac48c93e8027b8897d6babb2cd8e3267fed
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2018-11-24 02:31:57 -0800

    Create armsnos.java

---
+++ armsnos.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class armsnos{
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int low = sc.nextInt(); 
+		int high = sc.nextInt();
+
+        for(int number = low + 1; number < high; ++number) {
+            int digits = 0;
+            int result = 0;
+            int originalNumber = number;
+
+            // number of digits calculation
+            while (originalNumber != 0) {
+                originalNumber /= 10;
+                ++digits;
+            }
+
+            originalNumber = number;
+
+            // result contains sum of nth power of its digits
+            while (originalNumber != 0) {
+                int remainder = originalNumber % 10;
+                result += Math.pow(remainder, digits);
+                originalNumber /= 10;
+            }
+
+            if (result == number)
+                System.out.print(number + " ");
+        }
+    }
+}
