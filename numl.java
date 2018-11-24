85c6011196af71f55e6cb2d048bba724fc7fa551
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2018-11-24 01:48:55 -0800

    Create numl.java

---
+++ numl.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class numl
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();int m=1;
+		for(int i=1;i<=5;i++)
+		{
+			m=n*i;
+			System.out.print(m+ " ");
+		}
+	}
+}	
