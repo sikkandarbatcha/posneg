46068af4be06e9280f4343ff024b0aaea9e83251
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2018-11-26 04:03:17 -0800

    Create arrlar.java

---
+++ arrlar.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class arrlar 
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i] = sc.nextInt();
+		}
+		int temp = a[0] ;
+		for(int i=1;i<n;i++ )
+		{
+			if(temp<a[i])
+			{
+				temp = a[i];
+			}		
+					
+		}
+		System.out.println(temp);
+		
+	}
+}	
+				
