e4aa96ea5695dfdc9f36a7fb0a44683f8b32c1a6
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2018-11-27 03:53:52 -0800

    Create linecount.java

---
+++ linecount.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+class linecount
+{
+	public static void main(String args[])throws Exception
+	{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String str = bf.readLine();
+		int len = str.length();
+		int count = 1;
+		for(int i =0;i<len;i++)
+		{
+			if((str.charAt(i))== '.')
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}	
+			
