784d3f4c2e18a65ef72a444a2f87d6c47031d76c
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2018-12-17 01:46:26 -0800

    Create inclusive.java

---
+++ inclusive.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class inclusive
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if((n>=1)&&(n<=10))
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
