c339c904863cb91e18806ab7020f9bde463a7f08
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2018-12-17 02:01:37 -0800

    Create nextdigit.java

---
+++ nextdigit.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class nextdigit
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int nxt = n + 1;
+		System.out.println(nxt);
+	}
+}	
