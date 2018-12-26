2417939eb26d72a050f78084ec90b38ab2359979
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2018-12-26 02:59:48 -0800

    Create nuch.java

---
+++ nuch.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class nuch
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		switch(n)
+		{
+			case 1: System.out.print("One");
+			break;
+			
+			case 2: System.out.print("Two");
+			break;
+			
+			case 3: System.out.print("Three");
+			break;
+			
+			case 4: System.out.print("Four");
+			break;
+			
+			case 5: System.out.print("Five");
+			break;
+			
+			case 6: System.out.print("Six");
+			break;
+			
+			case 7: System.out.print("Seven");
+			break;
+			
+			case 8: System.out.print("Eight");
+			break;
+			
+			case 9: System.out.print("Nine");
+			break;
+			
+			case 10: System.out.print("Ten");
+			break;
+			
+			default: System.out.print("Enter bw 1 to 10");
+			break;
+		}
+	}
+}	
