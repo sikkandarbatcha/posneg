68a824508a2e4b14f8c7efa47c4eaf7754fc9ad3
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2018-12-26 04:33:17 -0800

    Create ks.java

---
+++ ks.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class ks 
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int k= sc.nextInt();
+		
+		
+		int sum=0;
+		for(int i=0;i<=k;i++)
+		{
+			sum+=i;
+		}
+		System.out.println(sum);
+	}	
+}	
