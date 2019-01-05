0b67f63a0450fe3caa2a233e2a9a484a5cbda1a7
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2019-01-05 01:30:44 -0800

    Create twopower.java

---
+++ twopower.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class twopower
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.print(n*2);	
+	}
+}	
