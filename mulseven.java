e2ce04a3a9977cf1eeaadea24bb3e082c58d8da4
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2019-01-05 01:22:51 -0800

    Create mulseven.java

---
+++ mulseven.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class mulseven
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+			if(n%7==0)
+			{
+				System.out.print("yes");
+			}
+			else
+			{
+				System.out.print("no");
+			}		
+	}
+}	
