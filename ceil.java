e30037999965388023cecc5c880bb91f2188741f
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2019-02-01 01:58:59 -0800

    Create ceil.java

---
+++ ceil.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class ceil
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		float chunkSize  = sc.nextFloat();
+		
+		System.out.print(""+Math.ceil(chunkSize));
+	}
+}	
