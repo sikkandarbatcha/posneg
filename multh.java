33854d5696a7d935a655e4f12189af14fe59ebd5
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2019-02-01 02:10:45 -0800

    Create multh.java

---
+++ multh.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class multh
+{
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    if(n%13==0)
+    {
+      System.out.print("Yes");
+     }
+     else
+      System.out.print("No");
+  }
+ } 
