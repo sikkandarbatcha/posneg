ba24617e10b097f4e67da58ddb7f53042d832bf8
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2019-02-04 12:54:12 +0530

    Create si.java

---
+++ si.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class si
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int p = sc.nextInt();
+		int t = sc.nextInt();
+		int r = sc.nextInt();
+		int sim = (p*t*r)/100;
+		System.out.print(sim);
+	}
+}	
