8a9f408324ec5775747d32a06c7a653fbc7ceca5
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2019-02-04 12:40:48 +0530

    Create kcs.java

---
+++ kcs.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class kcs
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int c = sc.nextInt();
+		int k = c+273;
+		System.out.print(k);
+	}
+}	
