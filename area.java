250e5efb9e4c9eea416dd8cfe27fa8d7b623ff4f
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2019-05-09 14:45:23 +0530

    Create area.java

---
+++ area.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class area
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		float l = sc.nextFloat();
+		float b = sc.nextFloat();
+		float are = l*b;
+		System.out.format("%.5f", are);
+	}
+}	
