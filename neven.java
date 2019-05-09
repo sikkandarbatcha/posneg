12c9aaf39a5608b17027d8863195c8ef9203672c
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2019-05-09 14:31:17 +0530

    Create neven.java

---
+++ neven.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class neven
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n%2==0)
+		{
+			System.out.print(n);
+
+		}
+		else
+		{
+			System.out.print(n-1);
+		}
+		
+	}
+}	
