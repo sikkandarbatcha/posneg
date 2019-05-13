0dfd3ec1307626c568c7606dfc018f571f404778
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2019-05-13 13:17:53 +0530

    Create camelcase.java

---
+++ camelcase.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+class camelcase
+{
+	public static void main(String args[])throws Exception
+	{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String str = bf.readLine();
+		int len = str.length();
+		String s = ""; String ss="";
+		
+		Scanner lineScan = new Scanner(str); 
+         while(lineScan.hasNext()) {
+             String word = lineScan.next(); 
+             s += Character.toUpperCase(word.charAt(0)) + word.substring(1) + " "; 
+         }
+		System.out.println(s.trim());
+	}
+}
