204643af27ce0ad5a60e490ba12a52607b1a27d0
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2019-05-13 16:53:23 +0530

    Create smalcf.java

---
+++ smalcf.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class smalcf
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int res=0;
+		res= lcm(a,b);
+		System.out.print(res);
+	}
+	static int lcm(int x,int y)
+	{
+		return x*y/ggccdd(x,y);
+	}	
+	static int ggccdd(int m, int n)
+	{
+		if(m==0)
+			return m;
+		
+		if(n==0)
+			return n;
+		
+		if(m == n)
+			return m;
+		
+		if(m>n)
+			return ggccdd(m-n,n);
+		else
+			return ggccdd(m,n-m);
+			
+	}
+}	
